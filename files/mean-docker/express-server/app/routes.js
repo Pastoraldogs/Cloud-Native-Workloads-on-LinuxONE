3883a98201bb332279a3fb59cdc9f0f354999438
Pastoraldogs <36719475+Pastoraldogs@users.noreply.github.com>
2019-05-28 16:18:32 +0800

    Add files via upload

--- files/mean-docker/express-server/app/routes.js
+++ files/mean-docker/express-server/app/routes.js
@@ -1,13 +1,12 @@
 var Todo = require('./models/todo');
+var User = require('./models/user')
 
 function getTodos(res) {
     Todo.find(function (err, todos) {
-
         // if there is an error retrieving, send the error. nothing after res.send(err) will execute
         if (err) {
             res.send(err);
         }
-
         res.json(todos); // return all todos in JSON format
     });
 };
@@ -27,6 +26,7 @@ module.exports = function (app) {
         // create a todo, information comes from AJAX request from Angular
         Todo.create({
             text: req.body.text,
+            value: req.body.value,
             done: false
         }, function (err, todo) {
             if (err)
@@ -37,6 +37,43 @@ module.exports = function (app) {
         });
 
     });
+
+    app.get('/api/login', function (req, res) {
+        // 查询数据库，用户名是否存在，以及密码是否匹配
+        // req.body.name
+        // req.body.password
+        // res.send() 返回true 或者 false
+    });
+
+    app.get('/api/info', function (req, res) {
+        // 查询数据库，根据用户名
+        // req.body.name
+        // res.send() 返回余额，理财金额
+        User.find(function (err, users) {
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err) {
+                res.send(err);
+            }
+            res.json(users); // return all todos in JSON format
+        });
+    })
+
+    app.post('/api/deposit', function (req, res) {
+        // 存钱
+        // req.body.amount
+    })
+
+    app.post('/api/withdraw', function (req, res) {
+        // 取钱
+        // req.body.amount
+    })
+
+    app.post('/api/buy', function (req, res) {
+        // 购买理财产品
+
+    })
+
+
 
     // delete a todo
     app.delete('/api/todos/:todo_id', function (req, res) {
@@ -49,6 +86,7 @@ module.exports = function (app) {
             getTodos(res);
         });
     });
+
 
     // application -------------------------------------------------------------
     app.get('*', function (req, res) {
