d86c14216b83414da8f8cd386930adcb190e1308
Pastoraldogs <781200173@qq.com>
2019-05-30 10:43:37 +0800

    5-30 10:43

--- files/mean-docker/express-server/public/js/controllers/bank.js
+++ files/mean-docker/express-server/public/js/controllers/bank.js
@@ -8,7 +8,17 @@ angular.module('bankController', [])
         var tempList = $scope.state.split("&")
         $scope.formData.name = tempList[0].split("=")[1]
         $scope.formData.password = tempList[1].split("=")[1]
-
+        Services.login($scope.formData)
+            // if successful creation, call our get function to get all the new todos
+            .success(function (data) {
+                $scope.loading = false;
+                $scope.userData = data;
+                if ($scope.userData.length !== 0) {
+                    window.location.href = "http://baidu.com";
+                    $scope.state = "登录成功"
+                } else
+                    $scope.state = "登录失败"
+            });
         // CREATE ==================================================================
         // when submitting the add form, send the text to the node API
         $scope.login = function () {
